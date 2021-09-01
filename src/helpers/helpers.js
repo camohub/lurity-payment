abefce0c27354432ebccf9c83926a302ba5b8b33
Čamaj Vladimír <vladimir.camaj@gmail.com>
2021-09-01 10:29:20 +0200

    helpers/helpers.js update

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,7 +1,5 @@
 
 window.setCurrency = function (price, currency) {
-    console.log('price, currency');
-    console.log(price, currency);
     return price .toLocaleString('sk-SK', {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
